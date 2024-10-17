73118b31ee16392f17b09356647beecb4a28eb52
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2024-10-17 07:57:56 +0330

    0.0.0
    ### Version 0.0.0 - Initial Release 🛠️
    - **Basic bot setup** with essential commands.
    - Integrated **voice channel functionality** for playing radio.
    - Added **AFK management** and **status updates**.
    - Configurable **.env file** for easy setup.
    - Initial **database options**: JSON, MySQL, MongoDB.
    
    Co-Authored-By: iTsSobhan <99456259+iTsSobhan@users.noreply.github.com>

--- src/commands/Music/pause.js
+++ src/commands/Music/pause.js
@@ -24,7 +24,6 @@ module.exports = {
       PermissionFlagsBits.SendMessages,
       PermissionFlagsBits.EmbedLinks,
       PermissionFlagsBits.Connect,
-      PermissionFlagsBits.ManageGuild,
       PermissionFlagsBits.Speak
     ]),
     dm_permission: false,
